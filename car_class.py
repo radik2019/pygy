81ffd5f2549be73e35900a49ed2187e165d9d2ff
radu <rdceb8@gmail.com>
2019-12-20 18:02:32 +0100

    ho aggiunto il prezzo della macchina

--- car_class.py
+++ car_class.py
@@ -2,17 +2,22 @@ class Car:
     
     year = 0
     
-    def __init__(self, motore, peso):
+    def __init__(self, motore, peso, prezzo):
         self.motore = motore
         self.peso = peso
+        self.prezzo = prezzo
     
     def old(self):
         self.year += 1
+        self.prezzo -= 100
+    
+    def price(self):
+        pass
 
 
 
-bmw = Car(600, 1600)
-audi = Car(587, 1200)
-mercedes = Car(598, 1455)
+bmw = Car(600, 1600, 1000)
+audi = Car(587, 1200, 1000)
+mercedes = Car(598, 1455, 1000)
 
-alfa = Car(656, 1100)
+alfa = Car(656, 1100, 1000)
