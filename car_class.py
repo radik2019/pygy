f67f0b19b5a315d433d218bd19a5af27d9f69d05
radu <rdceb8@gmail.com>
2019-12-20 15:33:40 +0100

    ci sono 2 macchine

---
+++ car_class.py
@@ -0,0 +1,16 @@
+class Car:
+    
+    year = 0
+    
+    def __init__(self, motore, peso):
+        self.motore = motore
+        self.peso = peso
+    
+    def old(self):
+        self.year += 1
+
+
+
+bmw = Car(600, 1600)
+audi = Car(587, 1200)
+
